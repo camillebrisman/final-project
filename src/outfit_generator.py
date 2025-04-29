b5356b83d3edb80956ae5df43d334982426268ea
camillebrisman <camille.brisman@gmail.com>
2025-04-29 15:29:31 -0500

    converts images into jpg and renames them

--- src/outfit_generator.py
+++ src/outfit_generator.py
@@ -1,4 +1,25 @@
+from PIL import Image
+import os
 import pygame
+
+
+def sort_items(folder):
+    folder_path = os.path.abspath(folder)
+    img_number = 1
+	
+    for filename in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, filename)
+		
+        img = Image.open(img_path)
+        converted_img = img.convert("RGB")
+
+        new_filename = f"{folder}_{img_number}.jpg"
+        new_path = os.path.join(folder_path, new_filename)
+
+        converted_img.save(new_path, "JPEG")
+        os.remove(img_path)
+
+        img_number += 1
 
 
 def run_program(screen, bg_tile):
@@ -18,6 +39,7 @@ def main():
 	resolution = (1920, 1080)
 	screen = pygame.display.set_mode(resolution)
 	bg_tile = (pygame.image.load("background/background.png").convert())
+	sort_items('tops')
 	run_program(screen, bg_tile)
 		
 
